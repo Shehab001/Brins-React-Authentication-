9d15402d9f36aec0ea56030d915051f94387d915
Md. Shehab Chowdhury <shehab15-11396@diu.edu.bd>
2022-10-26 16:26:11 +0600

    homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Item from "./components/Header/Item/Item";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <Item></Item>
+    </div>
+  );
 }
 
 export default App;
